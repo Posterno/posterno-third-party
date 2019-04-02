b574550b5a21e530b4cb4322eb480ffa38d06881
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-02 10:23:45 +0100

    Skip expired listings within jetpacks sitemap #14

--- integrations/jetpack.php
+++ integrations/jetpack.php
@@ -22,3 +22,21 @@ function pno_jetpack_add_listings_to_sitemap( $post_types ) {
 	return $post_types;
 }
 add_filter( 'jetpack_sitemap_post_types', 'pno_jetpack_add_listings_to_sitemap' );
+
+/**
+ * Skip expired listings within jetpacks sitemap.
+ *
+ * @param boolean $skip_post whether to skip a post or not.
+ * @param object  $post the post object.
+ * @return boolean
+ */
+function pno_jetpack_skip_expired_non_public_listings( $skip_post, $post ) {
+	if ( 'listings' !== $post->post_type ) {
+		return $skip_post;
+	}
+	if ( ! pno_are_expired_listings_public() && pno_is_listing_expired( $post->ID ) ) {
+		return true;
+	}
+	return $skip_post;
+}
+add_action( 'jetpack_sitemap_skip_post', 'pno_jetpack_skip_expired_non_public_listings', 10, 2 );
